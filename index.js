fafd5bdd88bdf279a46c48e772e19c48547676b7
UtkinG <utking@mail.ru>
2017-04-05 20:20:12 +1000

    Add forecast option; allow specifying a country code

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const config = require('./config');
+const forecastTemplate = require('./forecast_template');
 const template = require('./template');
 const program = require('commander');
 const http = require('http');
 const querystring = require('querystring');
 
 program
-  .version('1.0.0')
+  .version('1.0.1')
   .usage('<city_name> [options]')
-  .option('-c, --country', 'Set country code. Default "ru"', 'ru')
+  .option('-f, --forecast', 'Show 5 day forecast')
+  .option('-c, --country [code]', 'Set country code. Default "ru"', 'ru')
   .option('-v, --verbose', 'Verbose output. Default false')
   .parse(process.argv);
 
 const verboseMode = !!program.verbose;
 const cityName = program.args.length ? program.args.pop() : config.default_city;
+const requestPath = (program.forecast ? config.api_forecast_path : config.api_path);
 
 const request = {
   host: config.api_host,
-  path: config.api_path + '?' + querystring.stringify({
+  path: requestPath + '?' + querystring.stringify({
     appid: config.app_id,
     units: config.units,
     q: cityName
-  })
+  }) + (program.country ? (',' + program.country) : '')
 };
 
 let response = '';
@@ -37,7 +40,12 @@ let res = http.get(request, resp => {
       if (verboseMode) {
         console.log(data);
       } else {
-        console.log(template(data));
+        if (program.forecast) {
+          console.log(forecastTemplate(data));
+        } else {
+          console.log(template(data));
+        }
+
       }
     } catch (e) {
       console.log(new Error(e.message));
